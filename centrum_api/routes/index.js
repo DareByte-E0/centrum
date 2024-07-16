59754233811ea846b40db88a7bf45bddf4d8fc74
Emmaobi7 <emma46574@gmail.com4>
2024-07-16 17:58:46 +0100

    added backend servivce on render

--- centrum_api/routes/index.js
+++ centrum_api/routes/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const AppController = require('../controllers/AppController')
 const FileController = require('../controllers/FileController')
+const AiController = require('../controllers/AiController')
 const multer = require("multer");
 
 
@@ -12,5 +13,6 @@ router.get('/bebe', AppController.bebe);
 router.get('/read', AppController.read_file);
 router.post('/upload_files', upload.array("files"), FileController.uploadFlies)
 router.get('/files', FileController.getFiles);
+router.post('/chat', AiController.chat);
 
 module.exports = router;
